5add1b09e5db966781d14e28f0896a4af3890147
Sagar <acharyasaagar@gmail.com>
2024-06-01 13:58:21 +0300

    Adds mongodb User model Modifies session util to use mongodb User model

--- app/models/user.server.ts
+++ app/models/user.server.ts
@@ -1,63 +1,55 @@
-import type { Password, User } from "@prisma/client";
 import bcrypt from "bcryptjs";
+import { BSON } from "mongodb";
+import { z } from "zod";
 
-import { prisma } from "~/db.server";
+import { mongodb } from "~/utils/db.server";
 
-export type { User } from "@prisma/client";
+const UserSchema = z.object({
+  _id: z.instanceof(BSON.ObjectId).transform((value) => value.toHexString()),
+  createdAt: z.date().default(() => new Date()),
+  email: z.string(),
+  password: z.string(),
+});
 
-export async function getUserById(id: User["id"]) {
-  return prisma.user.findUnique({ where: { id } });
+const UserViewSchema = UserSchema.omit({ password: true });
+
+export type User = z.infer<typeof UserSchema>;
+export type UserView = z.infer<typeof UserViewSchema>;
+
+const userCollection = mongodb.db().collection<User>("users");
+
+export async function getUserById(_id: User["_id"]) {
+  const foundUser = await userCollection.findOne({ _id });
+  return foundUser ? UserViewSchema.parse(foundUser) : null;
 }
 
 export async function getUserByEmail(email: User["email"]) {
-  return prisma.user.findUnique({ where: { email } });
+  const foundUser = await userCollection.findOne({ email });
+  return foundUser ? UserViewSchema.parse(foundUser) : null;
 }
 
 export async function createUser(email: User["email"], password: string) {
   const hashedPassword = await bcrypt.hash(password, 10);
-
-  return prisma.user.create({
-    data: {
-      email,
-      password: {
-        create: {
-          hash: hashedPassword,
-        },
-      },
-    },
-  });
+  const userDocument = UserSchema.parse({ email, password: hashedPassword });
+  const inserted = await userCollection.insertOne(userDocument);
+  return UserViewSchema.parse(inserted.acknowledged ? userDocument : null);
 }
 
 export async function deleteUserByEmail(email: User["email"]) {
-  return prisma.user.delete({ where: { email } });
+  return await userCollection.deleteOne({ email });
 }
 
 export async function verifyLogin(
   email: User["email"],
-  password: Password["hash"],
+  password: User["password"],
 ) {
-  const userWithPassword = await prisma.user.findUnique({
-    where: { email },
-    include: {
-      password: true,
-    },
-  });
+  const userWithPassword = await userCollection.findOne({ email });
 
-  if (!userWithPassword || !userWithPassword.password) {
-    return null;
-  }
+  if (!userWithPassword || !userWithPassword.password) return null;
 
-  const isValid = await bcrypt.compare(
-    password,
-    userWithPassword.password.hash,
-  );
+  const isValid = await bcrypt.compare(password, userWithPassword.password);
 
-  if (!isValid) {
-    return null;
-  }
+  if (!isValid) return null;
 
-  // eslint-disable-next-line @typescript-eslint/no-unused-vars
-  const { password: _password, ...userWithoutPassword } = userWithPassword;
-
-  return userWithoutPassword;
+  return UserViewSchema.parse(userWithPassword);
 }
